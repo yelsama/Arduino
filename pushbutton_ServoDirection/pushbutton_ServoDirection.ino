f8aea35d4877ec7ead00d64ff045aba369b6aaea
Yassein Eltayeb Mohamed Idris <107728160+yelsama@users.noreply.github.com>
2022-08-31 11:29:13 +0400

    Update pushbutton_ServoDirection.ino

--- pushbutton_ServoDirection/pushbutton_ServoDirection.ino
+++ pushbutton_ServoDirection/pushbutton_ServoDirection.ino
@@ -4,8 +4,8 @@ int motspeed = 10;
 Stepper mystepmot(spr, 2,4,3,5);
 int pb = 7;
 int pbstatus = 1;
+int state = 1;
 int dirs = 2038 ;
-int wait = 500;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
@@ -14,18 +14,22 @@ digitalWrite(pb, HIGH);
 mystepmot.setSpeed(motspeed);
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-while (digitalRead(pb) == 0)
-{
-  pbstatus = 0;
-  delay(10);
-}
 Serial.println(pbstatus);
 Serial.println(dirs);
-if (pbstatus == 0)
-{
+  // for fully stop and delay with push use commented code:
+// while (digitalRead(pb) == 0)
+// {
+//   pbstatus = 0;
+//   delay(10);
+// }
+// if (pbstatus == 0)
+//   dirs *= -1;
+// mystepmot.step(dirs/60);
+// pbstatus = 1;
+
+pbstatus = digitalRead(pb);
+if (pbstatus == 0 && state == 1)
   dirs *= -1;
-}
 mystepmot.step(dirs/60);
-pbstatus = 1;
+state = pbstatus;
 }
